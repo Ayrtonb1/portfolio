7534e0a30cf01f6dcba0725be84d1171e6a19408
charliewturner <c.w.turner@hotmail.com>
2025-07-22 14:20:49 +0100

    Create navbar container

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,13 @@ import Language from "./Language";
 import ModeSelect from "./ModeSelect";
 
 function Navbar(): React.JSX.Element {
-  return <h1>Hi from Navbar</h1>;
+  return (
+    <div className="navbar">
+      <div className="navbar-name">Charles Turner</div>
+      <Language />
+      <ModeSelect />
+    </div>
+  );
 }
 
 export default Navbar;
