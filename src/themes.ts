ccc0a5a1806853919ea85ae2bdbbf250e724dc2d
charliewturner <c.w.turner@hotmail.com>
2025-07-22 14:43:07 +0100

    Initialise and fill out first version of the colour/darkmode ts file

---
+++ src/themes.ts
@@ -0,0 +1,66 @@
+export const themes = {
+  initial: {
+    light: {
+      background: "#FFFAE3",
+      text: "#002642",
+      "accent-one": "#840032",
+      "accent-two": "#E59500",
+      "accent-three": "#02040F",
+    },
+    dark: {
+      background: "#002642",
+      text: "#FFFAE3",
+      "accent-one": "#840032",
+      "accent-two": "#E59500",
+      "accent-three": "#02040F",
+    },
+  },
+  vibrant: {
+    light: {
+      background: "#EADEDA",
+      text: "#2E294e",
+      "accent-one": "#820263",
+      "accent-two": "#d90368",
+      "accent-three": "#ffd400",
+    },
+    dark: {
+      background: "#2E294e",
+      text: "#EADEDA",
+      "accent-one": "#820263",
+      "accent-two": "#d90368",
+      "accent-three": "#ffd400",
+    },
+  },
+  pastel: {
+    light: {
+      background: "#f2f3d9",
+      text: "#151e3f",
+      "accent-one": "#0300027",
+      "accent-two": "#dc9e82",
+      "accent-three": "#c16e70",
+    },
+    dark: {
+      background: "#151e3f",
+      text: "#f2f3d9",
+      "accent-one": "#0300027",
+      "accent-two": "#dc9e82",
+      "accent-three": "#c16e70",
+    },
+  },
+  green: {
+    light: {
+      background: "#248232",
+      text: "#040f0f",
+      "accent-one": "#fcfffc",
+      "accent-two": "#248232",
+      "accent-three": "#2d3a3a",
+    },
+    dark: {
+      background: "#040f0f",
+      text: "#248232",
+      "accent-one": "#fcfffc",
+      "accent-two": "#248232",
+      "accent-three": "#2d3a3a",
+    },
+  },
+};
