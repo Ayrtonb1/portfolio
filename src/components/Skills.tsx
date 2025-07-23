1d1ec36c23cd9778bfccd47ac7e2338014113524
charliewturner <c.w.turner@hotmail.com>
2025-07-23 18:29:39 +0100

    Change CSS of Skills containers

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,33 +3,39 @@ import { skills } from "./skills";
 function Skills(): React.JSX.Element {
   return (
     <div className="skills-container">
-      <h1>Skills</h1>
-      <div>Frontend</div>
-      <ul className="skill-list">
-        {skills.frontend.map(({ name, icon: Icon }) => (
-          <li key={name} className="skill-item">
-            <Icon />
-          </li>
-        ))}
-      </ul>
-      <div>Backend</div>
+      <div className="skills-sub-container">
+        <h1>Skills</h1>
+        <h3 className="skills-heading">Frontend</h3>
+        <ul className="skill-list">
+          {skills.frontend.map(({ name, icon: Icon }) => (
+            <li key={name} className="skill-item">
+              <Icon />
+            </li>
+          ))}
+        </ul>
+      </div>
 
-      <ul className="skill-list">
-        {skills.backend.map(({ name, icon: Icon }) => (
-          <li key={name} className="skill-item">
-            <Icon />
-          </li>
-        ))}
-      </ul>
-      <div>Tooling & Workflow</div>
+      <div className="skills-sub-container">
+        <h3 className="skills-heading">Backend</h3>
+        <ul className="skill-list">
+          {skills.backend.map(({ name, icon: Icon }) => (
+            <li key={name} className="skill-item">
+              <Icon />
+            </li>
+          ))}
+        </ul>
+      </div>
 
-      <ul className="skill-list">
-        {skills.tooling.map(({ name, icon: Icon }) => (
-          <li key={name} className="skill-item">
-            <Icon />
-          </li>
-        ))}
-      </ul>
+      <div className="skills-sub-container">
+        <h3 className="skills-heading">Tooling & Workflow</h3>
+        <ul className="skill-list">
+          {skills.tooling.map(({ name, icon: Icon }) => (
+            <li key={name} className="skill-item">
+              <Icon />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
