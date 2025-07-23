188b0c475bcbc4cc6ff69bb4dbe5c83f75cad9d4
charliewturner <c.w.turner@hotmail.com>
2025-07-23 18:01:59 +0100

    Map icons into skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,12 +1,36 @@
+import { skills } from "./skills";
+
 function Skills(): React.JSX.Element {
   return (
     <div className="skills-container">
       <div>Frontend</div>
-      <ul></ul>
+      <ul className="skill-list">
+        {skills.frontend.map(({ name, icon: Icon }) => (
+          <li key={name} className="skill-item">
+            <Icon fontSize={"4rem"} />
+          </li>
+        ))}
+      </ul>
       <div>Backend</div>
-      <ul></ul>
+      <ul className="skill-list">
+        <ul className="skill-list">
+          {skills.backend.map(({ name, icon: Icon }) => (
+            <li key={name} className="skill-item">
+              <Icon fontSize={"4rem"} />
+            </li>
+          ))}
+        </ul>
+      </ul>
       <div>Tooling & Workflow</div>
-      <ul></ul>
+      <ul className="skill-list">
+        <ul className="skill-list">
+          {skills.tooling.map(({ name, icon: Icon }) => (
+            <li key={name} className="skill-item">
+              <Icon fontSize={"4rem"} />
+            </li>
+          ))}
+        </ul>
+      </ul>
     </div>
   );
 }
