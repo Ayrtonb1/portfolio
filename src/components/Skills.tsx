27bd2b192675c900ff509cfca298764609de36be
charliewturner <c.w.turner@hotmail.com>
2025-07-23 18:21:42 +0100

    Remove nested ul causing CSS issues

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,33 +3,32 @@ import { skills } from "./skills";
 function Skills(): React.JSX.Element {
   return (
     <div className="skills-container">
+      <h1>Skills</h1>
       <div>Frontend</div>
       <ul className="skill-list">
         {skills.frontend.map(({ name, icon: Icon }) => (
           <li key={name} className="skill-item">
-            <Icon fontSize={"4rem"} />
+            <Icon />
           </li>
         ))}
       </ul>
       <div>Backend</div>
+
       <ul className="skill-list">
-        <ul className="skill-list">
-          {skills.backend.map(({ name, icon: Icon }) => (
-            <li key={name} className="skill-item">
-              <Icon fontSize={"4rem"} />
-            </li>
-          ))}
-        </ul>
+        {skills.backend.map(({ name, icon: Icon }) => (
+          <li key={name} className="skill-item">
+            <Icon />
+          </li>
+        ))}
       </ul>
       <div>Tooling & Workflow</div>
+
       <ul className="skill-list">
-        <ul className="skill-list">
-          {skills.tooling.map(({ name, icon: Icon }) => (
-            <li key={name} className="skill-item">
-              <Icon fontSize={"4rem"} />
-            </li>
-          ))}
-        </ul>
+        {skills.tooling.map(({ name, icon: Icon }) => (
+          <li key={name} className="skill-item">
+            <Icon />
+          </li>
+        ))}
       </ul>
     </div>
   );
