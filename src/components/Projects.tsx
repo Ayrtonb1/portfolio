0d6ed8dd67021fa984879d213795953badf14267
charliewturner <c.w.turner@hotmail.com>
2025-07-23 19:36:44 +0100

    Add placeholder project cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,18 @@
 function Projects(): React.JSX.Element {
-  return <h1>Hi from Projects</h1>;
+  return (
+    <div className="projects-container">
+      <h1>Projects</h1>
+      <div className="project-tiles">
+        <div className="project-tile">
+          <h3>NC News</h3> Full-stack article-based web application
+        </div>
+        <div className="project-tile">
+          <h3>TUSE</h3> A real-time collaborative drawing web app made for my
+          Northcoders final group project.
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
