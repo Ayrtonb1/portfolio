beda077733a918cd8db4e47d2a420095d8f8a0bd
charliewturner <c.w.turner@hotmail.com>
2025-07-23 17:57:22 +0100

    Add icon and string references to skills.ts

--- src/components/skills.ts
+++ src/components/skills.ts
@@ -1,5 +1,45 @@
+import type { type JSX } from "react";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaReact,
+  FaNodeJs,
+  FaGitAlt,
+  FaGithub,
+} from "react-icons/fa";
+import {
+  SiJavascript,
+  SiPostgresql,
+  SiJest,
+  SiSocketdotio,
+  SiSupabase,
+  SiExpress,
+  SiKonva,
+} from "react-icons/si";
+import { TbBrandVscode } from "react-icons/tb";
+import type { IconType } from "react-icons";
+
+export type Skill = { name: string; icon: IconType };
+
 export const skills = {
-  frontend: [],
-  backend: [],
-  tooling: [],
+  frontend: [
+    { name: "HTML", icon: FaHtml5 },
+    { name: "JavaScript", icon: SiJavascript },
+    { name: "CSS", icon: FaCss3Alt },
+    { name: "React", icon: FaReact },
+    { name: "Konva", icon: SiKonva },
+  ],
+  backend: [
+    { name: "Node.js", icon: FaNodeJs },
+    { name: "Express", icon: SiExpress },
+    { name: "PostgreSQL", icon: SiPostgresql },
+    { name: "Supabase", icon: SiSupabase },
+    { name: "Socket.IO", icon: SiSocketdotio },
+  ],
+  tooling: [
+    { name: "Git", icon: FaGitAlt },
+    { name: "GitHub", icon: FaGithub },
+    { name: "Jest", icon: SiJest },
+    { name: "VS Code", icon: TbBrandVscode },
+  ],
 };
