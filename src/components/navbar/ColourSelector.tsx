4633076958d41e5a2693e30fffbf3b8c5ff2781c
charliewturner <c.w.turner@hotmail.com>
2025-07-23 10:28:29 +0100

    Bring useTheme into ColourSelector component

--- src/components/navbar/ColourSelector.tsx
+++ src/components/navbar/ColourSelector.tsx
@@ -1,6 +1,12 @@
 import { FaPalette } from "react-icons/fa";
+import { useState } from "react";
+import { useTheme } from "../ThemeContext";
 
 function ColourSelector(): React.JSX.Element {
+  const { palette, setPalette } = useTheme();
+
+  const palettes = ["initial", "vibrant", "pastel", "green"];
+
   return <FaPalette />;
 }
 
