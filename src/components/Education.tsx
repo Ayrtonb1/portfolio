42a49e343c1f64ac04682bf1fb0ee371e491a8c1
charliewturner <c.w.turner@hotmail.com>
2025-07-24 20:39:07 +0100

    Edit alignment of divs in skills tiles

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -4,16 +4,21 @@ function Education(): React.JSX.Element {
       <h1>Education</h1>
       <div className="education-tiles">
         <div className="education-tile">
-          <h3>IB Diploma</h3> Wellington International School (dates)
+          <h3>IB Diploma</h3> Wellington International School <br />
+          2013-2015
         </div>
         <div className="education-tile">
           <h3>BSc Biochemistry</h3> University of Leicester
+          <br />
+          2016-2019
         </div>
         <div className="education-tile">
-          <h3>Software Development Bootcamp</h3> Northcoders
+          <h3>Software Development Bootcamp</h3> Northcoders <br />
+          03/2025 - 07/2025
         </div>
         <div className="education-tile">
-          <h3>Web Development Self Study</h3> The Odin Project
+          <h3>Web Development Self Study</h3> The Odin Project <br />
+          2023 - Present
         </div>
       </div>
     </div>
