f15202cfeab6a0d481b3735028ff085ffcac43c9
charliewturner <c.w.turner@hotmail.com>
2025-07-24 13:04:57 +0100

    Change max body width

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -6,12 +6,12 @@ function Background(): React.JSX.Element {
       <h1>My journey to date</h1>
 
       <div className="journey-wrapper">
-        <div className="journey-line"></div>
+        <div className="journey-line" />
         <div className="journey-timeline">
           {events.map((event, index) => (
             <div className="journey-entry" key={index}>
               <div className="journey-year"> {event.year}</div>
-              {/* <div></div> */}
+              <div className="journey-dot" />
               <div className="journey-content">{event.content}</div>
             </div>
           ))}
