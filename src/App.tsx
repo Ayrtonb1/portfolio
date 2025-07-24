67a5539c117074ac68987e19fca57fe068b66461
charliewturner <c.w.turner@hotmail.com>
2025-07-24 21:38:18 +0100

    Componentise colour palette examples for later use. Remove from active render

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Education from "./components/Education";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import ColourExample from "./components/ColourExample";
 import { ThemeProvider } from "./components/ThemeContext";
 
 function App(): React.JSX.Element {
@@ -14,13 +15,7 @@ function App(): React.JSX.Element {
         <div className="backdrop">
           <div className="content-background">
             <Navbar />
-            <div className="colour-container">
-              <div className="colour-example" id="c-1"></div>
-              <div className="colour-example" id="c-2"></div>
-              <div className="colour-example" id="c-3"></div>
-              <div className="colour-example" id="c-4"></div>
-              <div className="colour-example" id="c-5"></div>
-            </div>
+            {/* <ColourExample /> */}
             <MainInfo />
             <Background />
             <Education />
