fc614611887ed795fc86938d60f8f426e920ed73
charliewturner <c.w.turner@hotmail.com>
2025-07-24 11:00:29 +0100

    Commit before tailwind install

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -1,5 +1,10 @@
 function Background(): React.JSX.Element {
-  return <h1>My journey to date</h1>;
+  return (
+    <>
+      <h1>My journey to date</h1>
+      <h3>A short intro into my timeline</h3>
+    </>
+  );
 }
 
 export default Background;
