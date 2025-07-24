14ae6d32edf410682295b96e920278ed80f268c6
charliewturner <c.w.turner@hotmail.com>
2025-07-24 12:55:25 +0100

    Create events ts file for timeline

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -1,7 +1,21 @@
+import { events } from "./events";
+
 function Background(): React.JSX.Element {
   return (
     <>
       <h1>My journey to date</h1>
+
+      <div className="journey-wrapper">
+        <div className="journey-line"></div>
+        <div className="journey-timeline">
+          {events.map((event, index) => (
+            <div key={index}>
+              {event.year}
+              {event.content}
+            </div>
+          ))}
+        </div>
+      </div>
       <h3>A short intro into my timeline</h3>
     </>
   );
---
+++ src/components/background/events.ts
@@ -0,0 +1,6 @@
+export const events = [
+  { year: 2019, content: "2019 content" },
+  { year: 2020, content: "2020 content" },
+  { year: 2021, content: "2021 content" },
+  { year: 2025, content: "2025 content" },
+];
