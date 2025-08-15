436f80e237ec33b8b0e2de886a010bccb88b57df
charliewturner <c.w.turner@hotmail.com>
2025-08-15 14:56:03 +0100

    Add link behaviour to project section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,18 +3,28 @@ function Projects(): React.JSX.Element {
     <div className="projects-container">
       <h1>Projects</h1>
       <div className="project-tiles">
-        <div className="project-tile">
+        <a
+          href="https://ct-nc-news.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="project-tile"
+        >
           <h3>NC News</h3> A full-stack Reddit-style news and commenting
           platform built with React, Express, Node.js, and PostgreSQL. Features
           include article sorting, commenting, voting, and user authentication.
           Hosted on Render.
-        </div>
-        <div className="project-tile">
+        </a>
+        <a
+          href="https://tusemain.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="project-tile"
+        >
           <h3>TUSE</h3> A real-time collaborative drawing web app created for my
           final Northcoders group project. Built with React, Socket.IO, and
           Konva for canvas drawing. Includes user syncing, shape tools, and
           undo/redo support.
-        </div>
+        </a>
       </div>
     </div>
   );
--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -54,7 +54,7 @@ function Background(): React.JSX.Element {
   return (
     <>
       <h1>My journey to date</h1>
-      <h3>A short intro into my timeline</h3>
+      {/* <h3>A short intro into my timeline</h3> */}
       <div className="journey-wrapper" ref={containerRef}>
         <div className="journey-line-wrapper">
           <div className="journey-line">
